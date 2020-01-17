01347f994b9ee94121cbb451c5967f29dee40e5d
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2020-01-17 11:59:23 +0100

    Upgrade to the latest solidus_dev_support
    Override some extension files with the new generated ones.

--- Gemfile
+++ Gemfile
@@ -1,14 +1,23 @@
 # frozen_string_literal: true
 
 source 'https://rubygems.org'
+git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 branch = ENV.fetch('SOLIDUS_BRANCH', 'master')
 gem 'solidus', github: 'solidusio/solidus', branch: branch
 
+# Needed to help Bundler figure out how to resolve dependencies,
+# otherwise it takes forever to resolve them.
+# See https://github.com/bundler/bundler/issues/6677
+gem 'rails', '>0.a'
+
+# Provides basic authentication functionality for testing parts of your engine
+gem 'solidus_auth_devise'
+
 case ENV['DB']
 when 'mysql'
   gem 'mysql2'
-when 'postgres'
+when 'postgresql'
   gem 'pg'
 else
   gem 'sqlite3'
--- lib/generators/solidus_abandoned_carts/install/install_generator.rb
+++ lib/generators/solidus_abandoned_carts/install/install_generator.rb
@@ -10,19 +10,11 @@ module SolidusAbandonedCarts
       end
 
       def run_migrations
-        if running_migrations?
+        run_migrations = options[:auto_run_migrations] || ['', 'y', 'Y'].include?(ask('Would you like to run the migrations now? [Y/n]'))
+        if run_migrations
           run 'bundle exec rake db:migrate'
         else
-          puts "Skiping rake db:migrate, don't forget to run it!"
-        end
-      end
-
-      private
-
-      def running_migrations?
-        options.auto_run_migrations? || begin
-          response = ask 'Would you like to run the migrations now? [Y/n]'
-          ['', 'y'].include? response.downcase
+          puts 'Skipping rake db:migrate, don\'t forget to run it!' # rubocop:disable Rails/Output
         end
       end
     end
