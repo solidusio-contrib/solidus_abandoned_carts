718b92c7a52daa51f1ece7e0d98f90944e7797f3
Ben A. Morgan <ben@benmorgan.io>
2021-01-28 16:16:10 -0700

    do not create a partial index for mysql dbs

--- db/migrate/202101261212121_add_abandoned_cart_indexes_to_spree_orders.rb
+++ db/migrate/202101261212121_add_abandoned_cart_indexes_to_spree_orders.rb
@@ -3,10 +3,15 @@
 class AddAbandonedCartIndexesToSpreeOrders < SolidusSupport::Migration[5.1]
   def change
     add_index :spree_orders, [:email, :updated_at]
-    add_index :spree_orders, :updated_at, where: <<~SQL.squish
-      email IS NOT NULL
-      AND completed_at IS NULL
-      AND item_count > 0
-    SQL
+
+    if ActiveRecord::Base.connection.adapter_name == 'MySQL'
+      add_index :spree_orders, [:updated_at, :email, :completed_at, :item_count]
+    else
+      add_index :spree_orders, :updated_at, where: <<~SQL.squish
+        email IS NOT NULL
+        AND completed_at IS NULL
+        AND item_count > 0
+      SQL
+    end
   end
 end
